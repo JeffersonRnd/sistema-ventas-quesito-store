2e5d131979a58f27239ad3fb1586f93d51624bde
Tu Nombre <bazanjefferson60@gmail.com>
2026-06-02 01:20:27 -0500

    Primer commit

---
+++ sistema-ventas-quesito-store/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using sistema_ventas_quesito_store.Models;
+using System.Diagnostics;
+
+namespace sistema_ventas_quesito_store.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
